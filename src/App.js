87a81e38c0debd66b64a713a13434821fdd3adfb
mirzamitdinovs <sayyodmirzomitdinov@gmail.com>
2022-10-12 15:05:20 +0500

    codes removed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
-import AddTodo from './components/AddTodo';
-import TodoList from './components/TodoList';
 
 function App() {
-	return (
-		<div className='bg-gray-100 h-screen '>
-			<div className='w-1/2 mx-auto py-20'>
-				<TodoList />
-			</div>
-		</div>
-	);
+	return <div className='bg-gray-100 h-screen '>app file</div>;
 }
 
 export default App;
